b2efd2ccecfff305163fb96c96a5e845c24e69af
stuefi94 <btc.stefanfaderl@gmail.com>
2022-03-16 17:15:14 +0100

    implemented board features

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import { Board } from "./js/Board.js";
 
 // console.log(Board.whichGame());
-let board = new Board(350, 300); // in px
+let board = new Board(700, 450); // in px
 // console.log(board.SayCoordinates());
 // console.log(board.allCoordinates);
 // board.newCoordinates = { x: 100, y: 250 };
 // console.log(board.allCoordinates);
 
-board.drawBoard();
+board.drawBoard(board);
--- js/Board.js
+++ js/Board.js
@@ -21,6 +21,46 @@ export class Board {
     return "Tic Tac Toe";
   }
 
-  drawBoard() {
+  drawBoard(boardSize) {
+    // 1. HTML Objekte erzeugen und hinzufügen -> sehr effiziente und umfangreichste Methode
+    console.log(boardSize.x);
+    console.log(boardSize.y);
+
+    const main = document.querySelector("main");
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("board-size");
+    newDiv.style.width = `${boardSize.x}px`;
+    newDiv.style.height = `${boardSize.y}px`;
+    //newDiv.textContent = "Seas";
+    main.append(newDiv);
+
+    printFields();
+
+    /*     const ul = document.querySelector(".liste");
+    const newListItem = document.createElement("li"); // erstellt sowas: <li></li> dem Liste element wird somit der Text übergeben, den Inhalt
+    // so: <li>Der Rettich ist scharf</li>
+    newListItem.textContent = "Der Rettich ist scharf";
+    // so: <li class="special">Der Rettich ist scharf</li>
+    newListItem.classList.add("special"); // klasse übergeben
+
+    newListItem.addEventListener("click", () => {
+      // () ist quasi wie Function () ohne namen
+      console.log("Hallo, ich bins der Rettich!");
+    });
+
+    ul.append(newListItem);
+    // wenn mans z.B. oben haben möchte in der ul, gibt´s auch prepend
+    ul.prepend(newListItem);
+    ul.append(newListItem); */
+  }
+}
+
+function printFields() {
+  const newDiv = document.querySelector(".board-size");
+  // noch mit anderen for loops probieren
+  for (let i = 1; i < 10; i++) {
+    const fields = document.createElement("div");
+    fields.classList.add(`field-${i}`);
+    newDiv.appendChild(fields);
   }
 }
